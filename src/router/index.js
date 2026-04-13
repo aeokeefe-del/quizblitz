c8d80533d39ddf97cb923781db342eca9dd3b333
aeokeefe-del <aeokeefe@wisc.edu>
2026-04-13 21:39:23 +0200

    Update nav guard to use store

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useGameStore } from '../stores/gameStore.js'
 import HomeView from '../views/HomeView.vue'
 import PlayView from '../views/PlayView.vue'
 import LeaderboardView from '../views/LeaderboardView.vue'
 
+const routes = [
+  { path: '/', name: 'home', component: HomeView },
+  { path: '/play', name: 'play', component: PlayView },
+  { path: '/leaderboard', name: 'leaderboard', component: LeaderboardView }
+]
+
 const router = createRouter({
   history: createWebHistory(),
-  routes : [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/play', name: 'play', component: PlayView },
-    { path: '/leaderboard', name: 'leaderboard', component: LeaderboardView }
-  ]
+  routes
+})
+
+router.beforeEach((to) => {
+  if (to.name === 'play') {
+    const store = useGameStore()
+    if (store.gameState !== 'playing') {
+      return { name: 'home' }
+    }
+  }
 })
 
 export default router
